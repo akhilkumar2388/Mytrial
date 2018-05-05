af35a4dc5c57059d6fec52ee29e3b1c85cf8869c
Akhil Rohal <Akhil Rohal@AKHILROHAL-PC>
2018-05-05 19:57:12 +0530

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+        IWebDriver driver = new ChromeDriver();
+        [TestMethod]
+        public void login(string username, string password)
+        {
+            driver.FindElement(By.Name("uid")).SendKeys(username);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.Name("btnlogin")).Submit();
+
+            if (driver.Title == "Guru99 Bank Manager HomePage")
+            {
+                Console.WriteLine(" at homepage");
+
+            }
+            else { Console.WriteLine("home page not found"); }
+            Assert.IsTrue(driver.FindElement(By.Name("btnlogin")).Displayed);
+           }
+        }
+    }
+
---
+++ selenium/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.Extensions;
+using OpenQA.Selenium.Support.UI;
+namespace selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { meth1(); }
+            catch(Exception ex) { Console.WriteLine(ex.StackTrace); }
+            Console.ReadKey();
+        }
+
+         public static void meth1()
+        {
+            try { meth2(); }
+            catch (Exception ex) { throw ex; }
+        }
+        public static void meth2()
+        {
+            try { throw new Exception("trial exception"); }
+            catch (Exception ex) { throw; }
+        }
+    }
+        // driver.TakeScreenshot().SaveAsFile(@"C:\Users\desktop\error.jpg",ScreenshotImageFormat.Jpeg);
+
+    
+}
+    
+  
+
